d1492c489c539a9fd648de1e727e3970062128d7
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-08-06 17:06:33 +0200

    basic report page ui setup

--- components/Reports.js
+++ components/Reports.js
@@ -1,10 +1,46 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+import colors from "../config/colors";
 
 export default function Reports({ navigation }) {
   return (
-    <View>
-      <Text> Report Screen</Text>
+    <View style={styles.container}>
+      <Pressable onPress={console.log("Pressed report")} style={styles.buttons}>
+        <Text style={styles.buttonText}>Report Empty Bin</Text>
+      </Pressable>
+
+      <Pressable
+        onPress={console.log("Pressed emergency")}
+        style={styles.buttons}
+      >
+        <Text style={styles.buttonText}>Emergency Request</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttons: {
+    width: "50%",
+    height: "15%",
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20,
+    backgroundColor: colors.primaryButton,
+    borderRadius: 25,
+    borderColor: colors.primaryButtonBorder,
+    borderWidth: 2.5,
+  },
+
+  buttonText: {
+    fontSize: 18,
+  },
+});
---
+++ config/colors.js
@@ -0,0 +1,6 @@
+export default {
+  white: "#FFF",
+  black: "#000",
+  primaryButton: "#d4f3ee",
+  primaryButtonBorder: "#55B7AD",
+};
